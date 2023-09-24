775a3eea48b1a60960d2a38f4a87a7cb7507714a
Owen Roth <rothowen27@gmail.com>
2023-09-24 14:37:45 +0900

    homepage updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,12 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Spencer Sharp',
+    template: '%s - Owen Roth',
     default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+      'Owen Roth -  Software Developer, Fitness Enthusiast, and Navy Veteran.',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'I’m Owen, a software developer based in Chicago, IL. I specialize in building full stack web applications with Ruby and Javascript frameworks. Outside of tech, I frequently train for marathons, strength train, ski, travel, and take pictures.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -273,32 +273,32 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Software Developer, Fitness Enthusiast, and Navy Veteran.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
-          </p>
+          Hello! I'm Owen Roth, a software developer with expertise in Ruby and various JavaScript frameworks. My forte lies in backend development as I craft intricate full-stack applications that deliver seamless user experiences. 
+          <br/>
+          <br/>
+          Beyond the digital realm, I'm passionate about fitness, indulging in both running and strength training, and sometimes I moonlight as a photographer, capturing landscapes. Today, I'm channeling my skills and passion at Launchpad Lab, where we're at the forefront of creating innovative software solutions for a myriad of challenges.
+          </p> 
           <div className="mt-6 flex gap-6">
-            <SocialLink
+            {/* <SocialLink
               href="https://twitter.com"
               aria-label="Follow on Twitter"
               icon={TwitterIcon}
-            />
+            /> */}
             <SocialLink
-              href="https://instagram.com"
+              href="https://www.instagram.com/owen.roth.v1"
               aria-label="Follow on Instagram"
               icon={InstagramIcon}
             />
             <SocialLink
-              href="https://github.com"
+              href="https://github.com/oroth8"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
             />
             <SocialLink
-              href="https://linkedin.com"
+              href="https://www.linkedin.com/in/owen-roth-86ba1ba4"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
             />
