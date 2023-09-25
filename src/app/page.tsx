67fb0e773ea6578387d67e1e6550d18e546e9f26
Owen Roth <rothowen27@gmail.com>
2023-09-25 18:19:00 -0500

    add resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -237,7 +237,7 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
+      <Button href="/static/OWEN_ROTH_RESUME.pdf" download variant="secondary" className="group mt-6 w-full">
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
